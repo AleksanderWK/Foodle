c560cdd294d50a73d670ff5a5f6f909cff0f9461
AleksanderWK <aleksawk@stud.ntnu.no>
2022-02-20 17:53:19 +0100

    Update backend app.js

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,18 @@ const bodyParser = require("body-parser");
 const port = 3001;
 require("dotenv/config");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const usersRoute = require("./routes/users")
-app.use("/register", usersRoute)
+const usersRoute = require("./routes/users");
+app.use("/register", usersRoute);
 
-const groceriesRoute = require("./routes/groceries")
-app.use("/groceries", groceriesRoute)
+const groceriesRoute = require("./routes/groceries");
+app.use("/groceries", groceriesRoute);
 
 //CONNECT TO MONGODB
-mongoose.connect(process.env.MONGODB_URI, ()=>{
-    console.log("Connected to MongoDB!")
-})
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log("Connected to MongoDB!");
+});
 
 //LISTENING
-app.listen(port)
+app.listen(port);
