0c4de5abd2ca91a07d37f6e39a8bb0d0267048d3
AleksanderWK <aleksawk@stud.ntnu.no>
2022-07-25 21:22:55 +0200

    #28 Add backend support for add and get of consumptions

---
+++ backend/models/consumption.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const ConsumptionSchema = mongoose.Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  groceries: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Grocery",
+    },
+  ],
+  meals: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Meal",
+    },
+  ],
+  consumptionDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model(
+  "Consumption",
+  ConsumptionSchema,
+  "consumptions"
+);
---
+++ backend/routes/consumptions.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const Consumption = require("../models/Consumption");
+const router = express.Router();
+
+// /consumptions
+
+// post a consumption for a user
+
+router.post("/add", async (req, res) => {
+  try {
+    const newConsumption = new Consumption({
+      owner: req.body.owner,
+      groceries: req.body.groceries,
+      meals: req.body.meals,
+    });
+    await newConsumption.save();
+    Consumption.find({ owner: req.body.owner })
+      .populate("groceries")
+      .populate("meals")
+      .then((consumptions) => res.json(consumptions));
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+// get a users monthly consumptions
+
+router.get("/:userId/lastthirty", async (req, res) => {
+  try {
+    const currentDate = new Date();
+    const thirtyDaysAgo = new Date().setDate(currentDate.getDate() - 30);
+    Consumption.find({
+      owner: req.params.userId,
+      currentDate: { $gte: thirtyDaysAgo },
+    })
+      .populate("groceries")
+      .populate("meals")
+      .then((consumptions) => res.json(consumptions));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+// delete a consumption for a user
+
+// edit a consumption for a users
+
+module.exports = router;
