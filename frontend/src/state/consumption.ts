a2602eb2b2aa28c08685f164b921f7958b790fc7
AleksanderWK <aleksawk@stud.ntnu.no>
2022-07-30 16:29:11 +0200

    #28 Add sort daily consumptions by time

--- frontend/src/state/consumption.ts
+++ frontend/src/state/consumption.ts
@@ -16,6 +16,9 @@ export const dailyConsumptionsState = selector<Consumption[]>({
                 new Date(consumption.consumptionDate).setHours(0, 0, 0, 0) ==
                 today.setHours(0, 0, 0, 0)
         )
+        dailyConsumptions.sort((a, b) =>
+            a.consumptionDate > b.consumptionDate ? 1 : -1
+        )
         return dailyConsumptions
     },
 })
